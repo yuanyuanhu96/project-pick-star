0859e95b0daa2a2e369681b26d33ab2d35177535
yuanyuanhu96 <yhu76@id.iit.edu>
2020-04-04 01:57:07 -0500

    Created 2 objects at the same time

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,12 +4,14 @@ const ctx = canvas.getContext('2d');
 const claw = new Claw();
 //object = new Object(0, 20, 50, 60);
 const object = new Object(0, 20);
+const multiObjects = new MultiObjects(0, 20);
 
 const tick = () => {
   //console.log('Tick', tick);
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   claw.tick();
   object.tick();
+  multiObjects.tick();
 
   window.requestAnimationFrame(tick);
 };
---
+++ assets/js/multiObjects.js
@@ -0,0 +1,80 @@
+console.log('object loaded');
+
+class MultiObjects {
+  type;
+  radius;
+  x;
+  y;
+
+  /*I don't understand the difference between constructor(type, x, y, radius) and
+  x;
+  y;
+  radius;
+  type;
+ */
+  //？？？ What does constructor usually do? I didn't really make the constructer for claw
+  //Do I need to make right object wrong object different class
+  //???? if undefined why?
+
+  //draw a circle for each object for test
+
+  constructor(type, radius, x, y) {
+    console.log('constructing an object');
+    this.type = type;
+    this.radius = radius;
+
+    if (x !== undefined) {
+      this.x = x;
+    } else {
+      this.x = Math.round(Math.random() * canvas.width);
+    }
+
+    if (y !== undefined) {
+      this.y = y;
+    } else {
+      this.y = Math.round(Math.random() * canvas.height);
+    }
+
+    console.log(this);
+
+    this.drawCircle();
+  }
+
+  drawCircle() {
+    console.log(
+      'drawing a circle from the object',
+      this.x,
+      this.y,
+      this.radius
+    );
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+    ctx.strokeStyle = 'green';
+    ctx.lineWidth = 2;
+    ctx.stroke();
+  }
+
+  tick() {
+    this.drawCircle();
+  }
+}
+
+// class MultiObjects {
+//   needCreate;
+//   type;
+//   created = 0;
+
+//   constructor(needCreate, type) {
+//     this.needCreate = needCreate;
+//     this.type = type;
+//     this.createObjects();
+//   }
+
+//   createObjects() {
+//     if (this.created <= this.needCreate) {
+//       const object = new window.Object(0, 20);
+//       this.created += 1;
+//       this.createObjects();
+//     }
+//   }
+// }
