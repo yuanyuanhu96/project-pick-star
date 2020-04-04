433abc278a56da0b1f969f4178f82dab82da30f8
yuanyuanhu96 <yhu76@id.iit.edu>
2020-04-04 00:35:08 -0500

    finish constructor of object

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,6 +2,7 @@ const canvas = document.querySelector('#drawing');
 const ctx = canvas.getContext('2d');
 
 const claw = new Claw();
+const object = new Object(0, 20, 50, 60);
 
 const tick = () => {
   //console.log('Tick', tick);
--- assets/js/object.js
+++ assets/js/object.js
@@ -1,3 +1,26 @@
 console.log('object loaded');
 
+class Object {
+  type;
+  radius;
+  x;
+  y;
 
+  /*I don't understand the difference between constructor(type, x, y, radius) and
+  x;
+  y;
+  radius;
+  type;
+ */
+  //？？？ What does constructor usually do? I didn't really make the constructer for claw
+  //Do I need to make right object wrong object different class
+  //???? if undefined why?
+  constructor(type, radius, x, y) {
+    console.log('constructing an object');
+    this.type = type;
+    this.radius = radius;
+    this.x = x;
+    this.y = y;
+    console.log(this);
+  }
+}
