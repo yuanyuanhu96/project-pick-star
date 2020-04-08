b9746c32317f710975adea136869c91d7be42e5a
yuanyuanhu96 <yhu76@id.iit.edu>
2020-04-08 00:05:49 -0500

    finished return of claw

--- assets/js/claw.js
+++ assets/js/claw.js
@@ -1,4 +1,5 @@
 class Claw {
+  stars;
   status = 0;
   fixedEnd = { x1: 250, y1: 250 };
 
@@ -14,7 +15,17 @@ class Claw {
 
   flexibleEnd = { x2: 250, y2: 250 };
 
-  constructor(status, turningSpeed, turningTime, growingSpeed, growingTime) {
+  touchStar = false;
+
+  constructor(
+    stars,
+    status,
+    turningSpeed,
+    turningTime,
+    growingSpeed,
+    growingTime
+  ) {
+    this.stars = stars;
     this.draw();
   }
 
@@ -25,10 +36,19 @@ class Claw {
     if (status == 2) {
       this.growingTime += 1;
     }
+
+    if (status == 3) {
+      this.growingTime -= 1;
+    }
   }
 
   getLength() {
     let length = this.initialLength + this.growingSpeed * this.growingTime;
+
+    if (length < this.initialLength) {
+      status = 4;
+    }
+
     return length;
   }
 
@@ -40,6 +60,14 @@ class Claw {
   getEnd() {
     this.flexibleEnd.x2 = this.fixedEnd.x1 + this.length * Math.cos(this.angle);
     this.flexibleEnd.y2 = this.fixedEnd.y1 + this.length * Math.sin(this.angle);
+    if (
+      this.flexibleEnd.x2 <= 0 ||
+      this.flexibleEnd.x2 >= canvas.width ||
+      this.flexibleEnd.y2 <= 0 ||
+      this.flexibleEnd.y2 >= canvas.height
+    ) {
+      status = 3;
+    }
   }
 
   //draw path from fixedEnd to flexibleEnd
@@ -53,11 +81,28 @@ class Claw {
     ctx.stroke();
   }
 
+  ifTouchStar() {
+    console.log('touch star?', this.touchStar);
+
+    //need to end while true
+    for (var i = 0; i < stars.length; i++) {
+      let distanceX = Math.abs(this.stars[i].x - this.flexibleEnd.x2);
+      let distanceY = Math.abs(this.stars[i].y - this.flexibleEnd.y2);
+      if (distanceX < 20 && distanceY < 20) {
+        this.touchStar = true;
+        status = 3; //定义域是什么
+        console.log('touch star?', this.touchStar);
+      }
+    }
+    return this.touchStar;
+  }
+
   tick() {
     this.statusCheck();
     this.length = this.getLength();
     this.angle = this.getAngle();
     this.getEnd();
     this.draw();
+    this.ifTouchStar();
   }
 }
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,24 +1,11 @@
 const canvas = document.querySelector('#drawing');
 const ctx = canvas.getContext('2d');
 
-const claw = new Claw();
 var stars = createStars(0, 10);
+const claw = new Claw(stars);
 
-var touchStar = touchStar();
-console.log('touch star?', touchStar);
 
-function touchStar() {
-  touchStar = false;
-  //need to end while ture
-  for (var i = 0; i < stars.length; i++) {
-    distanceX = Math.abs(stars[i].x - claw.flexibleEnd.x2);
-    distanceY = Math.abs(stars[i].y - claw.flexibleEnd.y2);
-    if (distanceX < 20 && distanceY < 20) {
-      touchStar = ture;
-    }
-  }
-  return touchStar;
-}
+
 
 const tick = () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -27,7 +14,6 @@ const tick = () => {
   for (var i = 0; i < stars.length; i++) {
     stars[i].tick();
   }
-
 
   window.requestAnimationFrame(tick);
 };
